122dde91ee2cd94b9e36a441515aff2aac067d5c
Gildemberg Leite <gildembergleite@gmail.com>
2023-12-03 11:56:57 -0300

    feat: create findByTag controller

--- src/api/tag/controllers/tag.ts
+++ src/api/tag/controllers/tag.ts
@@ -4,4 +4,14 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::tag.tag');
+export default factories.createCoreController('api::tag.tag', () => ({
+  async findPostsByTag(ctx) {
+    try {
+      const slug = ctx.params.slug
+      const entries = await strapi.service('api::tag.tag').findBySlug(slug)
+      ctx.body = entries
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}));
--- src/api/tag/services/tag.ts
+++ src/api/tag/services/tag.ts
@@ -4,4 +4,19 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::tag.tag');
+export default factories.createCoreService('api::tag.tag', () => ({
+  async findBySlug(slug: string) {
+    const results = await strapi.db.query('api::tag.tag').findMany({
+      where: { slug },
+      populate: {
+        posts: {
+          populate: {
+            author: true
+          }
+        }
+      }
+    })
+    
+    return results
+  }
+}));
